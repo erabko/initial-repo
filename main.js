e7e96df9113a904211c537b6d0a57a24a6b862a8
Elena Rabko <rabkolena@gmail.com>
2021-09-19 17:47:00 +0300

    changes

---
+++ main.js
@@ -0,0 +1,10 @@
+const fs = require('fs-extra');
+
+fs.mkdirsSync('folder1')
+fs.writeFileSync('./folder1/textfile.txt', 'abc');
+fs.mkdirsSync('folder2')
+fs.moveSync('./folder1/textfile.txt', './folder2/textfile.txt');
+fs.emptyDirSync('./folder2')
+fs.removeSync('folder1')
+fs.removeSync('folder2')
+console.log('------');
