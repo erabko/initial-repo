334be991dbb9a2bc944dd8064f7d5c162ccae941
Elena Rabko <rabkolena@gmail.com>
2021-09-27 16:21:43 +0300

    Homework 4

--- main.js
+++ main.js
@@ -1,10 +1,50 @@
-const fs = require('fs-extra');
+// TASK 1.1: STRING+BOOLEAN
+console.log('it is ' + true);
+// expected output: it is true
 
-fs.mkdirsSync('folder1')
-fs.writeFileSync('./folder1/textfile.txt', 'abc');
-fs.mkdirsSync('folder2')
-fs.moveSync('./folder1/textfile.txt', './folder2/textfile.txt');
-fs.emptyDirSync('./folder2')
-fs.removeSync('folder1')
-fs.removeSync('folder2')
-console.log('--678---------');
+// TASK 1.2: STRING+NUMBER
+console.log('hello ' + 5);
+// expected output: hello 5
+
+// TASK 1.3: NUMBER+BOOLEAN
+console.log(2 + true);
+// expected output: 3
+
+// TASK 2.1: STRING*BOOLEAN
+console.log('text' * false);
+// expected output: NaN
+
+// TASK 2.2: STRING*NUMBER
+console.log('5' * 5);
+// expected output: 25
+
+// TASK 2.3: NUMBER*BOOLEAN
+console.log(10 * false);
+// expected output: 0
+
+// TASK 3.1: STRING/BOOLEAN
+console.log('text' / false);
+// expected output: NaN
+
+// TASK 3.2: STRING/NUMBER
+console.log('4' / 2);
+// expected output: 2
+
+// TASK 3.3: NUMBER/BOOLEAN
+console.log(10 / true);
+// expected output: 10
+
+// TASK 4.1: CONVERT TO NUMBER
+var a = '36';
+console.log(Number(a));
+// expected output: 36 as number
+
+// TASK 4.2: CONVERT TO SRTING
+var b = 100;
+console.log(String(b));
+// expected output: 100 as string
+
+// TASK 4.3: CONVERT TO BOOLEAN
+var c = 1;
+console.log(Boolean(c));
+// expected output: true
